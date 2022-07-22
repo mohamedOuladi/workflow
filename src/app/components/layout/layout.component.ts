36d7dcfcdda36ff8c0b907681c7bc341a2bfa369
artemnih <23387542+artemnih@users.noreply.github.com>
2022-07-22 13:14:53 -0400

    moving stuff around using mouse

--- src/app/components/layout/layout.component.ts
+++ src/app/components/layout/layout.component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewChild, ViewContainerRef } from '@angular/core';
+import { Component, ElementRef, ViewChild, ViewContainerRef } from '@angular/core';
 import { PluginAComponent } from '../plugin-a/plugin-a.component';
 
 @Component({
@@ -7,14 +7,23 @@ import { PluginAComponent } from '../plugin-a/plugin-a.component';
   styleUrls: ['./layout.component.scss']
 })
 export class LayoutComponent {
-
   @ViewChild('container', { read: ViewContainerRef }) container!: ViewContainerRef;
+  @ViewChild('containerEl', { read: ElementRef }) containerEl!: ElementRef;
+
+  isDragging = false;
+  draggedElement?: HTMLElement;
+  offsetX: number = 0;
+  offsetY: number = 0;
+
 
   dropped(event: DragEvent) {
-    const data = JSON.parse(event.dataTransfer!.getData('data'));
-    const compoonent = this.container.createComponent(PluginAComponent);
-    compoonent.instance.x =  event.clientX - data.x;
-    compoonent.instance.y =  event.clientY - data.y * 2; // temp hack
+    const dataJ = event.dataTransfer!.getData('data');
+    if (dataJ) {
+      const data = JSON.parse(dataJ);
+      const compoonent = this.container.createComponent(PluginAComponent);
+      compoonent.instance.x = event.clientX - data.x;
+      compoonent.instance.y = event.clientY - data.y * 2; // temp hack
+    }
 
     // TODO: instead of creating a new component, add it to the State and then render it
     // TODO: distinguish between new and existing components
@@ -22,5 +31,45 @@ export class LayoutComponent {
 
   allowDrop(event: any) {
     event.preventDefault();
+  }
+
+  mouseDown(e: MouseEvent) {
+    const element = (e.target as HTMLElement)!.closest('.plugin'); // TODO: classname from shared constant
+
+    // only certain elements can be dragged
+    if (element) {
+      this.isDragging = true;
+      this.draggedElement = element as HTMLElement;
+
+      // get coordinates of the element relative to the document
+      const rect = element.getBoundingClientRect();
+      const elementX = rect.left;
+      const elementY = rect.top;
+
+      // get mouse coordinates relative to the document
+      const mouseX = e.x;
+      const mouseY = e.y;
+      
+      // calculate the offset between the mouse and the element
+      this.offsetX = mouseX - elementX;
+      this.offsetY = mouseY - elementY;
+    }
+  }
+
+  mouseMove(e: MouseEvent) {
+    const containerClientX = this.containerEl.nativeElement.offsetLeft;
+    const containerClientY = this.containerEl.nativeElement.offsetTop;
+
+    if (this.isDragging && this.draggedElement) {
+      this.draggedElement.style.left = (e.clientX - containerClientX - this.offsetX) + 'px'
+      this.draggedElement.style.top = (e.clientY - containerClientY - this.offsetY ) + 'px'
+
+      // TODO: update the state of the component instead of setting style directly
+    }
+  }
+
+  mouseUp() {
+    this.isDragging = false;
+    this.draggedElement = undefined;
   }
 }
--- src/app/components/plugin-a/plugin-a.component.ts
+++ src/app/components/plugin-a/plugin-a.component.ts
@@ -1,20 +1,20 @@
-import { Component, ElementRef, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-plugin-a',
   templateUrl: './plugin-a.component.html',
   styleUrls: ['./plugin-a.component.scss']
 })
-export class PluginAComponent implements OnInit {
+export class PluginAComponent implements AfterViewInit {
   x: number = 0;
   y: number = 0;
 
-  constructor(private element: ElementRef) { }
+  @ViewChild('content', { read: ElementRef }) content!: ElementRef;
 
-  ngOnInit(): void {
-    this.element.nativeElement.style.position = 'absolute';
-    this.element.nativeElement.style.left = this.x + 'px';
-    this.element.nativeElement.style.top = this.y + 'px';
+  ngAfterViewInit(): void {
+    this.content.nativeElement.style.position = 'absolute';
+    this.content.nativeElement.style.left = this.x + 'px';
+    this.content.nativeElement.style.top = this.y + 'px';
   }
 
 }
