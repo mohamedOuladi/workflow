c444d2480c1a87d18957cea3e3680f9a157e1e85
artemnih <23387542+artemnih@users.noreply.github.com>
2022-07-26 13:31:03 -0400

    data passing update

--- src/app/directives/dynamic-plugin.directive.ts
+++ src/app/directives/dynamic-plugin.directive.ts
@@ -5,26 +5,29 @@ import { PluginAComponent } from '../components/plugin-a/plugin-a.component';
   selector: '[appDynamicPlugin]'
 })
 export class DynamicPluginDirective {
-  @Input() id!: number;
-  @Input() x!: number;
-  @Input() y!: number;
+  x!: number;
+  y!: number;
+
+  @Input() data?: { x: number, y: number, id: number };
   private component!: ComponentRef<PluginAComponent>;
 
   constructor(private viewContainerRef: ViewContainerRef) { }
 
   ngOnInit() {
     this.component = this.viewContainerRef.createComponent(PluginAComponent); // temp hardcoded component
-    this.component.instance.x = this.x;
-    this.component.instance.y = this.y;
-    this.component.instance.id = this.id || 0;
+    this.component.instance.x = this.data!.x;
+    this.component.instance.y = this.data!.y;
+    this.component.instance.id = this.data!.id || 0;
   }
 
   ngOnChanges(changes: any): void {
     if (this.component) {
-      this.x = changes.x?.currentValue || this.x;
-      this.y = changes.y?.currentValue || this.y;
+      this.x = changes.data.currentValue.x|| this.x;
+      this.y = changes.data.currentValue.y || this.y;
       this.component.instance.updatePosition(this.x, this.y);
     }
   }
 
 }
+
+// TODO: instead of moving the plugin, move the host. Convert this from a directive to a component.
