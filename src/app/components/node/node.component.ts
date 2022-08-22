3fdbe6de7ff752f164f94a56d35d744a4feb9fa5
artemnih <23387542+artemnih@users.noreply.github.com>
2022-08-22 14:16:12 -0400

    expand fixes

--- src/app/components/node/node.component.ts
+++ src/app/components/node/node.component.ts
@@ -1,6 +1,5 @@
 import { AfterViewInit, Component, ComponentRef, Input, OnChanges, OnInit, ViewChild, ViewContainerRef } from '@angular/core';
 import { PLUGINS } from 'src/app/plugins';
-import { GraphService } from 'src/app/services/graph.service';
 import { NodeX } from 'src/app/types';
 import { PluginAComponent } from '../plugin-a/plugin-a.component';
 
@@ -26,20 +25,17 @@ export class DynamicNodeComponent implements OnInit, AfterViewInit, OnChanges {
   hasOutlet = false;
   width = 250;
 
-  constructor(private graph: GraphService) {}
-
   ngOnInit(): void {
-    this.x = this.data!.x;
-    this.y = this.data!.y;
     this.id = this.data!.id || 0;
     this.type = this.data!.type;
     this.name = this.data!.name;
-    this.selected = this.data!.selected || false;
-    this.expanded = this.data!.expanded || false;
-    this.icon = PLUGINS.find((x) => x.type === this.type)!.icon;
-    this.hasInlet = PLUGINS.find((x) => x.type === this.type)!.hasInlet;
-    this.hasOutlet = PLUGINS.find((x) => x.type === this.type)!.hasOutlet;
-    this.width = PLUGINS.find((x) => x.type === this.type)!.width;
+    const plugin = PLUGINS.find((x) => x.type === this.type);
+    if (plugin) {
+      this.icon = plugin.icon;
+      this.hasInlet = plugin.hasInlet;
+      this.hasOutlet = plugin.hasOutlet;
+      this.width = plugin.width;
+    }
   }
 
   ngAfterViewInit(): void {
@@ -48,17 +44,11 @@ export class DynamicNodeComponent implements OnInit, AfterViewInit, OnChanges {
   }
 
   ngOnChanges(changes: any): void {
-    if (this.component) {
-      this.x = changes.data.currentValue.x || this.x;
-      this.y = changes.data.currentValue.y || this.y;
-      this.name = changes.data.currentValue.name || this.name;
-      this.selected = changes.data.currentValue.selected;
-      this.expanded = changes.data.currentValue.expanded;
-    }
+    this.x = changes.data.currentValue.x || this.x;
+    this.y = changes.data.currentValue.y || this.y;
+    this.name = changes.data.currentValue.name || this.name;
+    this.selected = changes.data.currentValue.selected;
+    this.expanded = changes.data.currentValue.expanded;
   }
 
-  expand(): void {
-    this.graph.expand(this.id);
-    console.log('expand', this.id);
-  }
 }
