088b6f108083a2b528766de23ad27fae6506bfe4
artemnih <23387542+artemnih@users.noreply.github.com>
2022-08-23 13:25:06 -0400

    copy paste

--- src/app/services/constants.service.ts
+++ src/app/services/constants.service.ts
@@ -2,12 +2,14 @@ import { InjectionToken } from '@angular/core';
 
 export interface Config {
   linkTopOffset: number;
+  duplicateOffset: number;
 }
 export const CONST = new InjectionToken<Config>('CONST', {
   providedIn: 'root',
   factory: (): Config => {
     return {
       linkTopOffset: 27,
+      duplicateOffset: 50,
     };
   },
 });
