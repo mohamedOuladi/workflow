bd04cafe0888366ff16a0ce3c7501767fc8d6032
mohamedOuladi <mohamed.ouladi@labshare.org>
2022-08-30 14:37:22 -0400

    added ls-auth

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService, SessionStorageService, WindowService } from '@labshare/base-ui-services';
+import { distinct } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,57 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'workflow';
+  public route: string;
+
+  constructor(
+    private authService: AuthService,
+    public sessionStorageService: SessionStorageService,
+    public windowService: WindowService,
+    private router: Router) {
+    this.route = this.getRedirectUrl();
+  }
+
+  async ngOnInit():  Promise<void> {
+    await this.authService.onAuthCallback();
+    /* Performing authorization with labshare auth*/
+    this.authService
+      .isAuthorized()
+      .pipe(distinct())
+      .subscribe(val => {
+        if (!val) {
+          this.sessionStorageService.setItem('route.after.login', this.route);
+          this.authService.login();
+        } else if (val === true) {
+          console.log(this.authService.getAccessToken());
+          const routeAfterLogin = this.sessionStorageService.getItem('route.after.login');
+          this.sessionStorageService.setItem('route.after.login', '');
+          const route = this.getRedirectUrl();
+          console.log('route: ' + route);
+          if (routeAfterLogin && routeAfterLogin !== route) {
+            this.router.navigate([routeAfterLogin]);
+          } 
+        }
+      });
+  }
+
+  /* Get main route */
+  getRedirectUrl() {
+    let afterLoginRoute: string;
+    afterLoginRoute = this.windowService.nativeWindow.location.href.replace(
+      this.windowService.nativeWindow.location.origin,
+      ''
+    );
+    afterLoginRoute =
+      afterLoginRoute.lastIndexOf('/') === afterLoginRoute.length - 1
+        ? afterLoginRoute.slice(0, afterLoginRoute.length - 1)
+        : afterLoginRoute;
+
+    if (!afterLoginRoute) {
+      return '';
+    }
+    if (afterLoginRoute.indexOf('/?code=') > -1 || afterLoginRoute.indexOf('?error=') > -1) {
+      return '';
+    }
+    return afterLoginRoute;
+  }
 }
